67dd615c30dfe760425b79c5276e93ae1d209ff2
Jakub <jakub.deka.dobrowolski@gmail.com>
2025-02-27 16:08:43 +0100

    added translate/translated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("YES");
+        Point punkt =new Point(2.3,4.5);
 
+        System.out.println(punkt.toString());
+        System.out.println(punkt.toSvg());
+        punkt.translate(2.3,4.5);
+        System.out.println(punkt.toSvg());
+        Point punkt2=punkt.translated(14,20);
+        System.out.println(punkt2.toSvg());
     }
 
 }
--- src/Point.java
+++ src/Point.java
@@ -1,9 +1,27 @@
-public class Point {
-    public int x;
-    public  int y;
+class Point {
+    public double x;
+    public  double y;
 
-    public Point(int x, int y) {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public String toString() {
+        return "Point(" + x + ", " + y + ")";
+    }
+    public String toSvg() {
+        return "<svg height=\"100\" width=\"100\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+                "  <circle r=\"45\" cx=\"" + x + "\" cy=\"" + y + "\" fill=\"red\" stroke=\"green\" stroke-width=\"3\" />\n" +
+                "</svg>";
+    }
+
+    public void translate(double dx, double dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public Point translated(double dx, double dy) {
+       return new Point(this.x + dx, this.y + dy);
     }
 }
